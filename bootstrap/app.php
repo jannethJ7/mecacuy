92ae0c80dcc2592be9af397f00e676da8cbb67b1
jannethJ7 <jannethchoque74@gmail.com>
2026-07-01 08:19:32 -0400

    htttps

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,13 +13,22 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+    //->withMiddleware(function (Middleware $middleware): void {
         //
+      //  $middleware->alias([
+      //        'iot.modulo' => \App\Http\Middleware\ModuloIotAuth::class,
+       //       'role' => \App\Http\Middleware\EnsureUserHasRole::class,
+       // ]);
+   // })
+   ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
-              'iot.modulo' => \App\Http\Middleware\ModuloIotAuth::class,
-              'role' => \App\Http\Middleware\EnsureUserHasRole::class,
+            'iot.modulo' => \App\Http\Middleware\ModuloIotAuth::class,
+            'role' => \App\Http\Middleware\EnsureUserHasRole::class,
         ]);
     })
+    
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (TokenMismatchException $e, Request $request) {
             if ($request->expectsJson() || $request->is('api/*')) {
